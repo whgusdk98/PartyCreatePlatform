92b5911da748b9ae5664d2466f2bd05a895e2783
Minjin Kim <kim003512@naver.com>
2019-11-03 05:13:43 +0900

    Merge branch 'master' of https://github.com/CHOI-SEUNGJUN/PartyCreatePlatform into origin
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/colors.xml

---
+++ app/src/main/java/com/sopt/partyflatform/NetworkService.kt
@@ -0,0 +1,11 @@
+package com.sopt.partyflatform
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NetworkService {
+//    @GET("")
+//    fun getDetailStoryResponse(
+//
+//    ): Call<GetDetailStoryResponse>
+}
