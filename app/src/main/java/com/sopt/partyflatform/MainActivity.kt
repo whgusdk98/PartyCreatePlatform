80b3ec04703d82d34561f9b1c9798813e2c49925
seungjun <smartao@naver.com>
2019-11-02 23:11:36 +0900

    add gradle

--- app/src/main/java/com/sopt/partyflatform/MainActivity.kt
+++ app/src/main/java/com/sopt/partyflatform/MainActivity.kt
@@ -2,11 +2,14 @@ package com.sopt.partyflatform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
